bd5dc7af15747fd8c40c244b6e5265304ed0e4cb
yaxing.chen <chenyaxing@rt-thread.com>
2020-05-11 18:53:36 +0800

    update : use board_cfg.h

---
+++ src/baremetal-port/drivers/drv_clk.c
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2006-2019, RT-Thread Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2019-10-26     ChenYong     first version
+ * 2020-01-08     xiangxistu   add HSI configuration
+ * 2020-04-20     chenyaxing   add system_clock_config frequency parameter
+ */
+
+#include <stm32f0xx.h>
+#include <stdio.h>
+
+void _error_handler(char *s, int num);
+
+#ifndef Error_Handler
+#define Error_Handler() _error_handler(__FILE__, __LINE__)
+#endif
+
+void system_clock_config(int target_freq_mhz)
+{
+    RCC_OscInitTypeDef RCC_OscInitStruct = { 0 };
+    RCC_ClkInitTypeDef RCC_ClkInitStruct = { 0 };
+
+    /** Initializes the CPU, AHB and APB busses clocks
+     */
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+    RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+    RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+    RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL12;
+    RCC_OscInitStruct.PLL.PREDIV = RCC_PREDIV_DIV2;
+    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+    {
+        Error_Handler();
+    }
+    /** Initializes the CPU, AHB and APB busses clocks
+     */
+    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1;
+    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+
+    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK)
+    {
+        Error_Handler();
+    }
+}
+
+int clock_information(void)
+{
+    printf("System Clock information\n");
+    printf("SYSCLK_Frequency = %d\n", (int) HAL_RCC_GetSysClockFreq());
+    printf("HCLK_Frequency   = %d\n", (int) HAL_RCC_GetHCLKFreq());
+    printf("PCLK1_Frequency  = %d\n", (int) HAL_RCC_GetPCLK1Freq());
+
+    return 0;
+}
+
